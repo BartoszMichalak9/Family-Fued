bed8574fc3198eee0ccc654ba0e40240681a4399
brettephillips <bep4144@rit.edu>
2016-05-13 18:10:46 -0400

    Comments on both he startup and player choice

--- PlayerChoice.java
+++ PlayerChoice.java
@@ -1,11 +1,31 @@
+/*
+ * @authors Tanner Glantz, Brett Phillips, Rosalee Hacker, and Alejandro Garzon
+ * @version 05/14/2016
+ * Description: The PlayerChoice class creates a window for the user to enter their name before starting the game.
+ * Course: ISTE-121
+ */
+
 //imports the necesary classes for the GUI program
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
 import java.awt.event.*;
 
+/*
+ * The PlayerChoice class creates a GUI for the users to enter their names into the game. After the user enters their name,
+ *    it creates a MainGUI object in order to start the game.
+ */
 public class PlayerChoice extends JFrame implements ActionListener
 {
+   /*
+    * @param JFrame frame Creates a JFrame object that will show the window that contains all of the panels.
+    * @param JLabel playerOneLabel Creates a JLabel object that that will let the user know what they need to type in the textbox.
+    * @param JTextField playerOneText Creates a JTextField object where the user will be able to enter his or her name as a player of the game.
+    * @param JButton ok Creates a JButton object that the user will press to add the players to the game and also start the game.
+    * @param JPanel panel Creates a JPanel object that will be in GridLayout and contain the JLabel and JTextField for the player input.
+    * @param JPanel panelTwo Creates a JPanel object that will be in FlowLayout and contain the JButton ok.
+    * @param String playerOneEntered Gets assigned the text that was put in the textField for the player.
+    */    
    private JFrame frame;
    private JLabel playerOneLabel = null;
    private JTextField playerOneText = null;
@@ -13,45 +33,78 @@ public class PlayerChoice extends JFrame implements ActionListener
    private JPanel panel = null;
    private JPanel panelTwo = null;
    private String playerOneEntered;
-   private String playerTwoEntered;
    
+   //PlayerChoice's main method
    public static void main(String [] args)
    {
+      //Creates a new PlayerChoice object
       PlayerChoice pC = new PlayerChoice();
    } 
      
+   /*
+    * The default constructor creates the GUI for the PlayerChoice class. It will create a frame that will
+    * have two panels added to it. These panels will contain JLabel and TextField objects that will allow
+    * the user to input their name and a JButton that will allow the user 
+    * accept the player, enter them, and start the game. 
+    */  
    public PlayerChoice()
    {
+      //Creates a JFrame object called frame
       frame = new JFrame();
+      //Sets the title of the frame
       frame.setTitle("Family Feud");
+      //Allows the user to close hte window and terminate the program by clicking the red X
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      //Sets the size of the frame
       frame.setSize(600, 200);
       
+      //Creates a new JPanel object called panel that is set to GridLayout
       panel = new JPanel(new GridLayout(2, 1));
       
+      //Creates a new JLabel object called playerOneLabel that takes in a String
       playerOneLabel = new JLabel("Player Name:");
+      //Adds playerOneLabel to panel
       panel.add(playerOneLabel);
+      //Creates a new JTextField object called playerOneText where the user will be able to enter his or her own name
       playerOneText = new JTextField();
+      //Adds playerOneText to panel
       panel.add(playerOneText);
             
+      //Creates a second JPanel called panelTwo which is defaultly FlowLayout
       panelTwo = new JPanel();
       
+      //Creates a new JButton object called ok that takes in a String
       ok = new JButton("OK");
+      //Adds the JButton ok to panelTwo
       panelTwo.add(ok);
+      //Adds an action to the button
       ok.addActionListener(this);
       
+      //Adds panelTwo to the frame and sets it to BorderLayout to the south of the frame
       frame.add(panelTwo, BorderLayout.SOUTH);
+      //Adds panel to the frame and sets it to BorderLayout to the north of the frame
       frame.add(panel, BorderLayout.CENTER);
+      //Sets the location of the frame to be in the middle of the page
       frame.setLocationRelativeTo(null);
+      //Sets the frame to be visible by the user
       frame.setVisible(true);
    } 
    
+   /*
+    * The actionPerformed() method listens for a call to the JButton Action Listener, then in return, performs
+    *       a certain action.
+    * @param ActionEvent ae Allows the action command to be performed 
+    */   
    public void actionPerformed(ActionEvent ae)
    {
+      //If the command equals OK, then it will perform the following action 
       if(ae.getActionCommand().equals("OK"));
       {
+         //Gets the text that was enterened in the TextField and assigns it to the String playerOneEntered
          playerOneEntered = playerOneText.getText();
+         //Creates a new MainGUIClient object and gives it a header of "player name: " and also takes in the playerOneEntered variable
          new MainGUIClient("Player Name: "+playerOneEntered);
+         //Sets the frame to false
          frame.setVisible(false);
       }
    }   
--- StartUp.java
+++ StartUp.java
@@ -1,47 +1,97 @@
+/*
+ * @authors Tanner Glantz, Brett Phillips, Rosalee Hacker, and Alejandro Garzon
+ * @version 05/14/2016
+ * Description: The StartUp class creates the start menu of the game. This class should be the only java file ran by the user
+ *              because it will call the PlayerChoice.java file which will then call the MainGUI.java.
+ * Course: ISTE-121
+ */
+
 //imports the necesary classes for the GUI program
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
 import java.awt.event.*;
 
+/*
+ * The startup class creates a GUI menu for the user to start the Family Feud Start menu.
+ * The user must press start when they are ready to start the game.
+ */
 public class StartUp extends JFrame implements ActionListener
 {
+   /*
+    * @param JFrame frame Creates a JFrame object that will show the window that contains all of the panels.
+    * @param JButton start Creates a JButton object that the user will press to start the game.
+    * @param JPanel panel Creates a JPanel object that will be in BorderLayout and contain the painted image.
+    * @param JPanel panelTwo Creates a JPanel object that will be in FlowLayout and contain the JButton start.
+    * @param Icon aPic Creates an Icon object that will take in the image to be shown.
+    */   
    private JFrame frame;
    private JButton start = null;
    private JPanel panel = null;
    private JPanel panelTwo = null;
    private Icon aPic = new ImageIcon("family-feud-logo.jpg");
       
+   //Startup's main method
    public static void main(String [] args)
    {
       //Creates a new StartUp object
       StartUp startUp = new StartUp();
    }
    
+   /*
+    * The default constructor that will create the GUI for the StartUp class. It will create a frame that will
+    * have two panels added to it. These panels will contain an Icon object to display an image
+    * and a JButton that will allow the user to start the game. 
+    */
    public StartUp()
    {
-      frame = new JFrame();
+      //Creates a JFrame object called frame
+      JFrame frame = new JFrame();
+      //Sets the size of the frame
       frame.setSize(588, 400);
+      //Sets the title of the frame
       frame.setTitle("Family Feud");
+      //Sets the location of the frame to be in the middle of the page
       frame.setLocationRelativeTo(null);
+      //Sets the frame so it cannot be resized
       frame.setResizable(false);
+      //Allows the user to close hte window and terminate the program by clicking the red X
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             
+      //Creates a JPanel object called panel that is in BorderLayout
       panel = new JPanel(new BorderLayout());
+      //Adds the inner class to panel
       panel.add(new StartUpInner());
+      //Adds panel to the frame
       frame.add(panel);
             
+      //Creates a JPanel object called panel that is in FlowLayout
       panelTwo = new JPanel(new FlowLayout());
+      //Creates a new JButton called start that takes in a String
       start = new JButton("Start");
+      //Adds an action to the button
       start.addActionListener(this);
+      //Adds the JButton to panelTwo
       panelTwo.add(start);
+      //Adds panelTwo to the frame and sets it in BorderLayout and to the South of the frame
       frame.add(panelTwo, BorderLayout.SOUTH);
                   
+      //Sets the frame to be visible by the user
       frame.setVisible(true);  
    }
    
+   /*
+    * Starup's inner class called Startupinner creates an image and paints it
+    *       it to the panel, so that it can be added to the frame in the 
+    *       main class. 
+    */   
    class StartUpInner extends JPanel
    {
+      /*
+       * The paintComponent() method accepts the Graphics g parameter in order to paint the 
+       * image to the panel at a specified location.
+       * @param Graphics g 
+       */      
       protected void paintComponent(Graphics g)
       {
          super.paintComponent(g);
@@ -49,12 +99,19 @@ public class StartUp extends JFrame implements ActionListener
       }
    }
       
+   /*
+    * The actoinPerformed() method listens for a call from the JButton's Action Listener in order to tell when a button is pressed.
+    * @param ActionEvent ae Allows the action command to be performed 
+    */    
    public void actionPerformed(ActionEvent ae)
    {
+      //If the command equals Start, then it will perform the following action 
       if(ae.getActionCommand().equals("Start"));
       {
+         //Creates a PlayerChoice object so that the next class can be started and the players playing can insert their names
          new PlayerChoice();
+         //Sets the frame to false
          frame.setVisible(false);
       }
-   }   
+   }    
 }  
